653fa583c8c4fb220ea5cab6b7fff639282af60c
BadWaka <1456683844@qq.com>
2017-11-04 18:08:58 +0800

    简单的服务器

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+// initial
+const app = express();
+
+// set static files path
+app.use('/static', express.static('static'));
+
+// routers
+app.get('/', function (req, res) {
+    res.send('hello world!');
+});
+
+// server
+const server = app.listen(3000, function () {
+    const host = server
+        .address()
+        .address;
+    const port = server
+        .address()
+        .port;
+    console.log(`Example app listening at http://${host}:${port}`);
+});
