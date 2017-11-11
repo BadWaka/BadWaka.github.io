f92981f926b6e6ea79b5c11005fe31303194ba22
BadWaka <1456683844@qq.com>
2017-11-11 23:20:50 +0800

    bak

--- crawler.js
+++ crawler.js
@@ -5,14 +5,6 @@ const console = require('tracer').colorConsole(); // 增强console
 const uaPC = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36';
 const uaMobile = 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1';
 
-axios.get('http://www.baidu.com')
-    .then((res) => {
-        console.log(res);
-    })
-    .catch((err) => {
-        console.error(err);
-    });
-
 axios({
     method: 'get',
     url: 'http://www.baidu.com',
