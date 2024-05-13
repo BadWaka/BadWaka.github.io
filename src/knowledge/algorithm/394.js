aa1d30df494325f1a6ff8e0326e01258fbb86359
BadWaka <1456683844@qq.com>
2024-05-13 21:02:06 +0800

    chore: 394

--- src/knowledge/algorithm/394.js
+++ src/knowledge/algorithm/394.js
@@ -36,12 +36,12 @@ function f(s) {
                 valMultiple += val;
             }
             console.log('valMultiple', valMultiple);
-            const oldStr = str.substring(l.index - k + 1, i + 1);
+            const oldStr = str.substring(l.index - (k - 1), i + 1);
             console.log('oldStr', oldStr);
             // 偏移，如果替换字符串的时候，新的字符串长了或者短了，i 需要重新移位
             const offset = valMultiple.length - oldStr.length;
             console.log('offset', offset);
-            str = str.substring(0, l.index - k + 1) + '' + valMultiple + str.substring(i + 1);
+            str = str.substring(0, l.index - (k - 1)) + '' + valMultiple + str.substring(i + 1);
             i += offset;
             console.log('str', str);
         }
@@ -50,8 +50,37 @@ function f(s) {
     return str;
 }
 
+function f2(s) {
+    const stackNum = [];
+    const stackChar = [];
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        // 不是数字
+        if (isNaN(+char)) {
+            stackChar.push(char);
+        }
+        // 是数字
+        else {
+            // 找到连续的数字
+            let numChar = char;
+            let j = i;
+            let nextChar = s[j + 1];
+            while(nextChar && !isNaN(nextChar)) {
+                numChar = numChar + '' + nextChar;
+                j++;
+                nextChar = s[j + 1];
+            }
+            console.log('j', j, 'numChar', numChar);
+            stackNum.push(numChar);
+            i = i + numChar.length - 1;
+        }
+    }
+    console.log('stackNum', stackNum);
+    console.log('stackChar', stackChar);
+}
+
 let params = '3[a2[c]]';
-// params = '3[a]2[bc]';
+params = '3[a]2[bc]';
 // params = '100[leetcode]';
-const res = f(params);
+const res = f2(params);
 console.log('res', res);
