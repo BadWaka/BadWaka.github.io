e7c1f40bc5dc77f7f3c2fb0d8738e06feae22bb8
BadWaka <1456683844@qq.com>
2024-06-20 19:19:23 +0800

    chore: AntDesignVue 按钮 Button 移动端点击异常

--- src/knowledge/issues/AntDesignVue 按钮 Button 移动端点击异常/index.vue
+++ src/knowledge/issues/AntDesignVue 按钮 Button 移动端点击异常/index.vue
@@ -2,11 +2,61 @@
 <template>
     <div>
         AntDesignVue 按钮 Button 移动端点击异常
+        <div class="flex">
+            <div class="left">
+                left
+            </div>
+            <div class="right">
+                <Button style="margin-right: 20px;" type="primary">按钮1</Button>
+                <Button type="primary" @click="showConfirm">出弹窗</Button>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { Button, Modal } from 'ant-design-vue';
+
+const showConfirm = () => {
+    Modal.confirm({
+        content: 'text'
+    });
+};
 </script>
 
 <style scoped lang="scss">
+
+// 导致问题的样式代码
+// .ant-btn:focus {
+//     // 禁止tab选中
+//     /* autoprefixer: ignore next */
+//     display: contents;
+// }
+
+// 修复代码
+/* 仅在支持 hover 的设备上应用，也就是 PC */
+// @media (hover: hover) and (pointer: fine) {
+//     .ant-btn:focus {
+//         // 禁止tab选中
+//         /* autoprefixer: ignore next */
+//         display: contents;
+//     }
+// }
+
+.flex {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    justify-content: space-between;
+    -webkit-box-pack: justify;
+    z-index: 9;
+}
+.left,
+.right {
+    display: flex;
+    align-items: center;
+    -webkit-box-align: center;
+}
 </style>
