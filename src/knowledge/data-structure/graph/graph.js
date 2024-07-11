5eabd5554ebf64d566cd1f90d6070ae9a57fc596
BadWaka <1456683844@qq.com>
2024-07-11 21:10:06 +0800

    chore: 浏览器自动换行，找到那个字符开始换行

--- src/knowledge/data-structure/graph/graph.js
+++ src/knowledge/data-structure/graph/graph.js
@@ -1,4 +1,52 @@
 
 class Graph {
 
+    adjMap;
+
+    constructor() {
+        // 邻接表
+        this.adjMap = {};
+    }
+
+    // 添加点
+    addVertex(v) {
+        this.adjMap[v] = [];
+    }
+
+    // 添加边
+    addEdge(v1, v2) {
+        this.adjMap[v1].push(v2);
+    }
+
+    // 拓扑排序
+    topologicalSort(map) {
+
+        if (!map) {
+            map = this.adjMap;
+        }
+        // console.log('map', map);
+
+        // 入度 Map
+        let inDegreeMap = {};
+        // 遍历邻接表，统计每个顶点的入度
+        Object.keys(map).forEach(vertex => {
+            const neighborList = map[vertex];
+            // console.log('vertex', vertex, 'neighborList', neighborList);
+            inDegreeMap[vertex] = neighborList.length;
+        });
+        // console.log('inDegreeMap', inDegreeMap);
+
+        // 入度为 0 的队列
+        let queueDegree0 = [];
+        // 结果数组
+        let res = [];
+
+    }
+
 }
+
+let graph = new Graph();
+graph.addVertex(3);
+graph.addVertex(6);
+graph.addEdge(3, 6);
+graph.topologicalSort();
---
+++ src/knowledge/issues/浏览器自动换行，找到那个字符开始换行/README.md
@@ -0,0 +1,31 @@
+# 浏览器自动换行，找到那个字符开始换行
+
+把 DOM 中的文案都用 span 包裹，再通过 getBoundingClientRect 方法计算位置
+
+```js
+/**
+ * 计算换行字符
+ */
+const calcWrapChar = () => {
+    console.log('calcWrapChar textDom.value', textDom.value);
+    let text = textDom.value.innerText;
+    console.log('text', text);
+
+    // 因为要拼 span 标签，所以必须使用 innerHTML；但是会导致样式没了
+    let newHTML = '';
+    // 将每个字符包裹在 <span> 标签中
+    for (let i = 0; i < text.length; i++) {
+        const char = text[i];
+        newHTML += `<span>${char}</span>`;
+    }
+    textDom.value.innerHTML = newHTML;
+    const children = textDom.value.children;
+    for (let i = 0; i < children.length; i++) {
+        const span = children[i];
+        const rect = span.getBoundingClientRect();
+        console.log('char', text[i], 'rect.y', rect.y);
+    }
+    textDom.value.innerHTML = text;
+
+};
+```
