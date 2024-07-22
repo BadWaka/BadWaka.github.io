4d11c829098d72e5c31638e4c037375f777fc4a9
BadWaka <1456683844@qq.com>
2024-07-22 16:19:14 +0800

    chore: deepClone 循环引用

--- src/knowledge/interview-questions/请用 js 实现一个深复制函数/deepClone.js
+++ src/knowledge/interview-questions/请用 js 实现一个深复制函数/deepClone.js
@@ -1,6 +1,15 @@
 
-export function deepClone(obj) {
-    console.log('typeof obj', typeof obj);
+export function deepClone(obj, hash = new WeakMap()) {
+    console.log('typeof obj', typeof obj, 'hash', hash);
+
+    if (obj === null) {
+        return null;
+    }
+
+    if (hash.has(obj)) {
+        console.log('有缓存');
+        return hash.get(obj);
+    }
 
     // 基础类型、函数和 Symbol
     if (typeof obj !== 'object') {
@@ -12,21 +21,46 @@ export function deepClone(obj) {
     if (Array.isArray(obj)) {
         console.log('数组');
         const newArray = [];
+        hash.set(obj, newArray);
         for (let i = 0; i < obj.length; i++) {
-            const newObj = deepClone(obj[i]);
+            const newObj = deepClone(obj[i], hash);
             newArray.push(newObj);
         }
         return newArray;
     }
 
     // 对象
+    let newObj = {};
+    hash.set(obj, newObj);
+    Object.keys(obj).forEach(key => {
+        newObj[key] = deepClone(obj[key], hash);
+    });
+    return newObj;
 
 }
 
-let test = {};
-// test = [];
+const a = {
+    val: 'a',
+};
+const b = {
+    val: 'b',
+    a: a
+};
+a.b = b;
+let test = {
+    a: a,
+    b: b
+};
+test = [
+    a,
+    b
+];
 // test = 1;
 // test = function () {
 // };
 // test = Symbol();
-deepClone(test);
+// test = null;
+// test = undefined;
+console.log('test', test);
+const res = deepClone(test);
+console.log('res', res);
