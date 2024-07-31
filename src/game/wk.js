a08224ee872b1ec710d2e3589842463ea44364a5
BadWaka <1456683844@qq.com>
2024-07-31 17:16:21 +0800

    chore: game

---
+++ src/game/wk.js
@@ -0,0 +1,21 @@
+
+const info = {
+    name: 'wk',
+    // 精力
+    energy: 100,
+    // 任务
+    mission: {
+        // 主线任务
+        main: {
+            target: 'find new job',
+            // 实现路径
+            path: [
+            ]
+        }
+    },
+    // 技能树
+    skillTree: {
+        // 前端
+        frontEnd: {}
+    }
+};
