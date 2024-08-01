2e040bc4cc14124c6ad6ca1e110654d37cf08d19
BadWaka <1456683844@qq.com>
2024-08-01 17:03:44 +0800

    chore: 236

---
+++ src/knowledge/data-structure/tree/tree.js
@@ -0,0 +1,62 @@
+
+/**
+ * 树节点
+ *
+ * @param {*} val
+ * @param {*} left
+ * @param {*} right
+ */
+export function TreeNode(val, left, right) {
+    this.val = val;
+    if (left) {
+        this.left = left;
+    }
+    if (right) {
+        this.right = right;
+    }
+}
+
+/**
+ * 生成二叉树，从数组
+ *
+ * @param {Array} nums
+ * @returns
+ */
+export function genBinaryTree(nums) {
+    if (nums === null || nums.length === 0 || nums[0] === null) {
+        return null;
+    }
+    let root = new TreeNode(nums[0]);
+    let queue = [root];
+    let i = 1;
+    while(i < nums.length) {
+        const item = nums[i];
+        const node = queue[0];
+        // console.log('\nitem', item, 'node', node);
+        if (node.left === undefined) {
+            if (!item) {
+                node.left = null;
+            }
+            else {
+                node.left = new TreeNode(item);
+                queue.push(node.left);
+            }
+        }
+        else if (node.right === undefined) {
+            if (!item) {
+                node.right = null;
+            }
+            else {
+                node.right = new TreeNode(item);
+                queue.push(node.right);
+            }
+            queue.shift();
+        }
+        else {
+            queue.shift();
+        }
+        i++;
+    }
+    // log('\nroot', root);
+    return root;
+}
---
+++ src/knowledge/leetcode/236.js
@@ -0,0 +1,16 @@
+
+import {
+    TreeNode,
+    genBinaryTree,
+} from '../data-structure/tree/tree.js';
+console.log('TreeNode', TreeNode, 'genBinaryTree', genBinaryTree);
+
+/**
+ * @returns
+ */
+function f(nums) {
+}
+
+let params = '';
+const res = f(params);
+console.log('\nres', res);
