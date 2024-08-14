fca9083316e9adc24b5cf78ea0a023017ea23887
BadWaka <1456683844@qq.com>
2024-08-14 14:45:44 +0800

    chore: fibonacci

--- src/knowledge/algorithm/dp/斐波那契数列/index.js
+++ src/knowledge/algorithm/dp/斐波那契数列/index.js
@@ -1,10 +1,25 @@
 
 /**
- * 
+ * 动态规划
  */
-function stack2Queue(nums) {
+function fibonacci(n) {
+    if (n === 0) {
+        return 0;
+    }
+    if (n === 1) {
+        return 1;
+    }
+    const dp = new Array(n + 1).fill(0);
+    // console.log('dp', dp);
+    dp[0] = 0;
+    dp[1] = 1;
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    console.log('dp', dp);
+    return dp[n];
 }
 
-let nums = [1, 2, 4, 7, 3, 5, 6, 8];
-const res = stack2Queue(nums);
+let n = 10;
+const res = fibonacci(n);
 console.log('res', res);
