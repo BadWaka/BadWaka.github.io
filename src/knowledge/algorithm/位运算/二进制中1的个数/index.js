f52798a1e305702c3a5ca95c8949c2e28ac31a73
BadWaka <1456683844@qq.com>
2024-08-19 16:36:09 +0800

    chore: 二进制中1的个数

--- src/knowledge/algorithm/位运算/二进制中1的个数/index.js
+++ src/knowledge/algorithm/位运算/二进制中1的个数/index.js
@@ -1,9 +1,9 @@
 
 /**
- * 首先要把十进制转换为二进制
- * 然后
+ * 首先要把十进制转换为二进制字符串
+ * 然后再遍历字符串比较是否等于 '1'
  */
-function binary1NumCount(n) {
+function binary1NumCount1(n) {
     let binaryString = '';
 
     while(n > 0) {
@@ -20,6 +20,37 @@ function binary1NumCount(n) {
             count++;
         }
     }
+    return count;
+}
+
+/**
+ * 位运算
+ * @param {*} n
+ */
+function binary1NumCount(n) {
+    // console.log('', n & 1);
+
+    // 整数向右移位
+    // let count = 0;
+    // while(n) {
+    //     if (n & 1 === 1) {
+    //         count++;
+    //     }
+    //     n = n >> 1;
+    // }
+    // return count;
+
+    // 标志位向左移位
+    let count = 0;
+    let flag = 1;
+    while(flag) {
+        console.log('flag', flag);
+        if (n & flag) {
+            count++;
+        }
+        flag = flag << 1;
+    }
+    console.log('flag', flag);
     return count;
 }
 
