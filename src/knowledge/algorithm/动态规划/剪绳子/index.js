4729c9d56f0ce2f88fea5fec8066cf0087b8c84b
BadWaka <1456683844@qq.com>
2024-08-19 11:05:11 +0800

    chore: 动态规划切绳子

--- src/knowledge/algorithm/动态规划/剪绳子/index.js
+++ src/knowledge/algorithm/动态规划/剪绳子/index.js
@@ -1,8 +1,30 @@
 
 /**
  * 动态规划
+ * n > 1
+ * m > 1
  */
 function cutRope(n) {
+    if (n === 2) return 1;
+    if (n === 3) return 2;
+
+    const dp = new Array(n + 1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 3;
+    for (let i = 4; i <= n; i++) {
+        let max = 0;
+        for (let j = 1; j < i; j++) {
+            const res = dp[j] * dp[i - j];
+            console.log('i', i, 'j', j, 'res', res);
+            if (res > max) {
+                max = res;
+            }
+        }
+        dp[i] = max;
+    }
+    console.log('dp', dp);
+    return dp[n];
 }
 
 let n = 10;
