1da251d3f71932f355db9d04921a8cf27d8d8ce6
BadWaka <1456683844@qq.com>
2024-08-23 19:49:40 +0800

    chore: 删除链表中重复的节点

--- src/knowledge/data-structure/linkedlist/删除链表中重复的节点/index.js
+++ src/knowledge/data-structure/linkedlist/删除链表中重复的节点/index.js
@@ -4,9 +4,31 @@ import {
 } from '../../../algorithm/util.js';
 
 /**
- * 
+ * 思路：
+ * 既然是排序的链表
+ * 那我从前往后遍历就好了
+ * 记录上一个 node 的值
+ * 如果当前 node 的值等于上一个 node 的值，就把当前 node 删掉
+ * 如果当前 node 的值不等于上一个 node 的值，就把上一个 node 的值更新然后继续找下一个
+ * 时间复杂度 O(n)
  */
 function deleteLinkedListRepeatNode(root) {
+    if (!root || !root.next) {
+        return root;
+    }
+    // 上一个 node 的值
+    let lastNode = root;
+    let node = root.next;
+    while(node) {
+        if (node.val === lastNode.val) {
+            lastNode.next = node.next;
+        }
+        else {
+            lastNode = node;
+        }
+        node = node.next;
+    }
+    log('', root);
 }
 
 let node = {
@@ -14,7 +36,7 @@ let node = {
     next: {
         val: 5,
         next: {
-            val: 6,
+            val: 5,
             next: null
         }
     }
@@ -23,7 +45,7 @@ let node = {
 let root = {
     val: 1,
     next: {
-        val: 2,
+        val: 1,
         next: {
             val: 3,
             next: node
