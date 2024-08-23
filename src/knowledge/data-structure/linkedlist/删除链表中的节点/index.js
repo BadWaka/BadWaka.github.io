d4727816cdcdda7f5d1b0f8cdd624a9aa591a167
BadWaka <1456683844@qq.com>
2024-08-23 19:33:39 +0800

    chore: 删除链表中的节点

--- src/knowledge/data-structure/linkedlist/删除链表中的节点/index.js
+++ src/knowledge/data-structure/linkedlist/删除链表中的节点/index.js
@@ -6,7 +6,11 @@ import {
 /**
  * 第一种方法时间复杂度是 O(n)，需要从头到尾顺序遍历链表
  *
- * 第二种方法是
+ * 第二种方法是找到 node 的下一个节点
+ * 把 node 的值换成它的下一个节点的值，把它的 next 换成它的下一个节点的 next
+ * 然后再把它的下一个节点删掉
+ *
+ * 如果是 node 是最后一个节点，那还得顺序遍历
  */
 function deleteLinkedListNode(root, node) {
     // log('', root);
