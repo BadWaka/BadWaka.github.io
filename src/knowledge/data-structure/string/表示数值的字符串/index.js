3b0864d5ec767b064d994e7c06b1c590af10892b
BadWaka <1456683844@qq.com>
2024-08-24 15:07:22 +0800

    chore: 表示数值的字符串

---
+++ src/knowledge/data-structure/string/表示数值的字符串/index.js
@@ -0,0 +1,16 @@
+
+/**
+ * A[.[B]][e|EC]
+ *
+ * A 为整数部分
+ * B 为小数部分
+ * C 为指数部分
+ *
+ * 首先要判断有没有正负号
+ */
+function isStrNumber(str) {
+}
+
+let str = '+100';
+const res = isStrNumber(str);
+console.log('res', res);
