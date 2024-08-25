4857c055da0a2d7cd8b39ee64c62fc61f91f9704
BadWaka <1456683844@qq.com>
2024-08-25 18:09:09 +0800

    chore: 反转链表

---
+++ src/knowledge/data-structure/linkedlist/反转链表/index.js
@@ -0,0 +1,67 @@
+
+import {
+    log
+} from '../../../algorithm/util.js';
+
+/**
+ */
+function reverse(root) {
+    log('', root);
+    if (!root || !root.next) {
+        return root;
+    }
+    let prefix = null;
+    let cur = root;
+    let next = cur.next;
+    while(cur) {
+        // 先把当前 node 的 next 指向前一个
+        cur.next = prefix;
+        // 再把前一个指向当前 node
+        prefix = cur;
+        // 当年 node 往前移
+        cur = next;
+        if (next) {
+            next = next.next;
+            cur.next = prefix;
+        }
+    }
+    return prefix;
+}
+
+// let node2 = {
+//     val: 7,
+//     next: {
+//         val: 8,
+//         next: {
+//             val: 9,
+//             next: null
+//         }
+//     }
+// };
+
+let node = {
+    val: 4,
+    next: {
+        val: 5,
+        next: {
+            val: 6,
+            next: null
+        }
+    }
+};
+
+// node2.next.next.next = node;
+
+let root = {
+    val: 1,
+    next: {
+        val: 2,
+        next: {
+            val: 3,
+            next: node
+        }
+    }
+};
+
+const res = reverse(root);
+log('res', res);
