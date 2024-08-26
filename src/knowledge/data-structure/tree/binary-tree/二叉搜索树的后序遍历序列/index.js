b903376c51bdae11326ed32f1f8aa3f136a66110
BadWaka <1456683844@qq.com>
2024-08-26 17:20:58 +0800

    chore: 二叉搜索树的后序遍历

---
+++ src/knowledge/data-structure/tree/binary-tree/二叉搜索树的后序遍历序列/index.js
@@ -0,0 +1,15 @@
+
+import {
+    log
+} from '../../../../algorithm/util.js';
+
+/**
+ * 思路：
+ */
+function isLRD(arr) {
+}
+
+let arr = [5, 7, 6, 9, 11, 10, 8];
+arr = [7, 4, 6, 5];
+const res = isLRD(arr);
+log('res', res);
