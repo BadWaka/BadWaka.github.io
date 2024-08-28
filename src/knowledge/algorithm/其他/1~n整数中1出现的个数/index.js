7f287cc266e9045f07aa5fdaa23712663bbed669
BadWaka <1456683844@qq.com>
2024-08-28 17:11:46 +0800

    bak

---
+++ src/knowledge/algorithm/其他/1~n整数中1出现的个数/index.js
@@ -0,0 +1,15 @@
+
+/**
+ * 思路：
+ * 暴力法
+ * 遍历从 1~n 所有数，转换为字符串，遍历每个字符，判断是否等于 '1'
+ * 如果等于 count++
+ * 最后 return count
+ */
+function times1(n) {
+}
+
+let n = 13;
+
+const res = times1(n);
+console.log('res', res);
