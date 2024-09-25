5ef4de8e2e3da3d979d18604e00960749e3bbe6e
BadWaka <1456683844@qq.com>
2024-09-25 10:17:10 +0800

    chore: bak

--- src/knowledge/js/curry/curry.js
+++ src/knowledge/js/curry/curry.js
@@ -1,18 +1,32 @@
 
 function curry(func) {
-    console.log('func', func, 'func.length', func.length);
     return function curried(...args) {
-        console.log('args', args);
+        // 判断当前参数是否 >= 函数参数
         if (args.length >= func.length) {
+            // 调用方法
             return func.apply(this, args);
         }
+        // 返回一个函数，继续接收参数
         return function(...moreArgs) {
-            console.log('moreArgs', moreArgs);
             return curried.apply(this, args.concat(moreArgs));
         };
     }
 }
 
+// function curry(func) {
+//     return function curried(...args) {
+//         // 判断当前参数长度是否 >= 函数的参数长度
+//         if (args.length >= func.length) {
+//             return func(...args);
+//         }
+//         // 返回一个函数，继续接收参数
+//         return function(...moreArgs) {
+//             return curried(...args.concat(moreArgs));
+//         };
+//     };
+// }
+
+// 测试
 function testFunc(a, b, c) {
     return a + b + c;
 }
@@ -22,3 +36,5 @@ const curriedFunc = curry(testFunc);
 console.log(curriedFunc(1)(2)(3));
 console.log(curriedFunc(1)(2, 3));
 console.log(curriedFunc(1, 2)(3));
+
+
--- src/knowledge/js/safeGet/index.js
+++ src/knowledge/js/safeGet/index.js
@@ -1,15 +1,8 @@
 
 function safeGet(obj) {
-    // 返回一个新的代理对象
-    // 第一个参数是对象
-    // 第二个参数是 handle
-
     const handler = {
         // 拿属性的操作
-        // 第一个参数是对象
-        // 第二个参数是属性名
         get(obj, prop) {
-            console.log('get obj', obj, 'prop', prop, prop in obj);
             // 判断属性是否在对象上
             if (prop in obj) {
                 // 如果是对象
@@ -17,21 +10,12 @@ function safeGet(obj) {
                     // 递归的调用自身
                     return safeGet(obj[prop]);
                 }
+                // 返回一个方法
                 return (value) => {
                     return value || obj[prop];
                 };
             }
             // 如果不存在
-            /**
-             * 解释：
-                new Proxy(() => undefined, handler)：这行代码创建了一个新的代理对象。这个代理的目标是一个箭头函数 () => undefined，该函数的作用是返回 undefined。
-
-                目的：当你尝试访问一个在原始对象上不存在的属性时，这行代码确保不会直接返回 undefined（因为如果你对 undefined 继续访问属性，会导致 TypeError）。相反，它返回一个新的代理，这个代理同样具备安全访问属性的能力。
-
-                重用处理器（Handler）：这个新的代理使用了相同的 handler。这意味着在这个函数代理上访问任何属性时，会再次检查该属性是否存在，如果不存在，则会返回另一个代理。这实现了无限制的安全属性访问链。
-
-                通过使用以函数为目标的代理，确保即使调用一个方法（例如 x.i.q.w.f.q.s.g()），它实际上返回的是 undefined（通过函数返回），而不会导致错误。这种设计允许对深层嵌套或不存在的属性进行安全访问，而不会意外地引发运行时错误。
-             */
             return new Proxy(() => undefined, handler);
         }
     };
